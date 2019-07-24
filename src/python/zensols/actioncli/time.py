5740492297a0332a508d4bc584084eb0b647001f
Paul Landes <landes@mailc.net>
2019-07-23 21:29:55 -0500

    func: add calling frame logger and support closure body f-strings

--- src/python/zensols/actioncli/time.py
+++ src/python/zensols/actioncli/time.py
@@ -4,6 +4,7 @@
 __author__ = 'Paul Landes'
 
 import logging
+import inspect
 import time as tm
 
 time_logger = logging.getLogger(__name__)
@@ -12,20 +13,52 @@ time_logger = logging.getLogger(__name__)
 class time(object):
     """Used in a ``with`` scope that executes the body and logs the elapsed time.
 
-    Example:
+    Format f-strings are supported as the locals are taken from the calling
+    frame on exit.  This means you can do things like:
 
-        with time("test1", logger):
+        with time('processed {cnt} items'):
+            cnt = 5
             tm.sleep(1)
 
+    which produeces: ``processed 5 items``.
+
+    See the initializer documentation about special treatment for global
+    loggers.
+
     """
-    def __init__(self, msg, logger=time_logger, level=logging.INFO):
+    def __init__(self, msg, logger=None, level=logging.INFO):
+        """Create the time object.
+
+        If a logger is not given, it is taken from the calling frame's global
+        variable named ``logger``.  If this global doesn't exit it uses it's
+        own logger defined in this module.
+
+        :param msg: the message log when exiting the closure
+        :param logger: the logger to use for logging
+        :param level: the level at which the message is logged
+
+        """
         self.msg = msg
         self.logger = logger
         self.level = level
+        frame = inspect.currentframe()
+        try:
+            globs = frame.f_back.f_globals
+            if 'logger' in globs:
+                self.logger = globs['logger']
+        except Exception as e:
+            time_logger.error(e)
 
     def __enter__(self):
         self.t0 = tm.time()
 
     def __exit__(self, type, value, traceback):
         elapse = tm.time() - self.t0
-        self.logger.log(self.level, f'{self.msg} finished in {elapse:.1f}s')
+        msg = self.msg
+        frame = inspect.currentframe()
+        try:
+            locals = frame.f_back.f_locals
+            msg = msg.format(**locals)
+        except Exception as e:
+            time_logger.error(e)
+        self.logger.log(self.level, f'{msg} finished in {elapse:.1f}s')
